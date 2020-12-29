9cbd0b4c5846e9f370f9d2d375ffb2f424f4180a
Radu-Alexandru Stoica <radu.stoica1994@gmail.com>
2020-12-29 14:59:10 +0100

    added posts section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,18 +5,26 @@ import logo from "../../logo.svg";
 export function Home() {
   return (
     <div className="container">
-      <div className="left">
-        <span id="title">Hi, I am John, Creative Technologist</span>
-        <span id="caption">
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-          sint. Velit officia consequat duis enim velit mollit. Exercitation
-          veniam consequat sunt nostrud amet.
-        </span>
-        <button>Download Resume</button>
-      </div>
-      <div className="right">
-        <img src={logo} alt="my pic" />
-      </div>
+      <section className="home">
+        <div className="left">
+          <span id="title">Hi, I am John, Creative Technologist</span>
+          <span id="caption">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </span>
+          <button>Download Resume</button>
+        </div>
+        <div className="right">
+          <img src={logo} alt="my pic" />
+        </div>
+      </section>
+      <section className="posts">
+        <div className="posts-title">
+          <span id="posts-recent">Recent posts</span>
+          <span id="posts-view-all">View all</span>
+        </div>
+      </section>
     </div>
   );
 }
